2c2ece1b971e9e790aabbe6d129806c49b3e748f
Eric Lamb <eric@mithra62.com>
2016-11-30 01:33:08 -0800

    updates namespace

--- src/Exceptions/PlatformsException.php
+++ src/Exceptions/PlatformsException.php
@@ -8,9 +8,9 @@
  * @version		1.0
  * @filesource 	./mithra62/Exceptions/PlatformsException.php
  */
-namespace mithra62\Exceptions;
+namespace JaegerApp\Exceptions;
 
-use mithra62\Exception;
+use JaegerApp\Exception;
 
 /**
  * mithra62 - Platforms Exception Object
